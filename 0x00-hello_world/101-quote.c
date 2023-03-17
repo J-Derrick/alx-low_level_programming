eade03b7a908a6ef95033f27e535741dc506eeec
my Name <me@gmail.com>
2023-03-17 01:21:31 -0700

    programming is life

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@
 int main(void)
 {
 	char err[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-        fwrite(&err, sizeof(char), 59, stderr);
-        return (1);
+	fwrite(&err, sizeof(char), 59, stderr);
+	return (1);
 }
