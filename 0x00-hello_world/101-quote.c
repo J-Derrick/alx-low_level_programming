5525dcefbaec75432034d5fc6e354a80afee020a
my Name <me@gmail.com>
2023-03-17 01:19:33 -0700

    programming is life

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <unistd.h>
+#include <stdio.h>
 /**
- * main - Entry point
- * Return: Always 0 (Success)
+ * main - output similar info as input
+ * Return: 1 (Success)
  */
-char main(void)
+int main(void)
 {
-	const c = ("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-
-	return (1);
+	char err[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+        fwrite(&err, sizeof(char), 59, stderr);
+        return (1);
 }
