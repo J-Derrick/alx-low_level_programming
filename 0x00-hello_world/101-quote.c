f5975eac692ac84b3697d942d610029a2853a883
my Name <me@gmail.com>
2023-03-16 19:26:41 -0700

    C programming

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,14 +4,9 @@
  *
  * Return: Always 0 (Success)
  *
- * /
+ */
 int main(void)
 {
-    const char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    ssize_t len = sizeof(msg) - 1;  // exclude the terminating null byte
-
-    write(STDERR_FILENO, msg, len);
-
+    const char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";. 59);
     return 1;
 }
-
