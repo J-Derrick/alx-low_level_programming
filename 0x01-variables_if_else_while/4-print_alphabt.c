e014e3d92eb5e88b8a21e01251c4f630bac614ac
my Name <me@gmail.com>
2023-03-20 04:20:13 -0700

    C variables and loop statements

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <ctype.h>
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (sucess)
+ */
+
+int main(void)
+{
+/* we use the ASCII table where a=97 and z=122 */
+	int lowercase = 'a';
+
+	while (lowercase <= 'z')
+	{
+		if (lowercase != ('q' || 'e'))
+		{
+			putchar(lowercase);
+			lowercase += 1;
+		}
+
+	}
+	putchar('\n');
+	return (0);
+}
