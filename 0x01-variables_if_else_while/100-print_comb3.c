b3f163e20514a796dcc3ffc3a372bb6e17b6fb76
my Name <me@gmail.com>
2023-03-20 12:12:47 -0700

    C variables and conditional loops

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - function used to prinnt a combination of two numbers
+ *
+ * Return: Always 0 (sucess)
+ */
+int main(void)
+{
+	int i = '0';
+	int j = '0';
+
+	while (i <= '9')
+	{
+		while (j <= '9')
+		{
+			/*The two digits must be different*/
+			if (!(i > j) || (i == j))
+			{
+				putchar(i);
+				putchar(j);
+				/* The two digits must be different*/
+				if (i == '8' && j == '9')
+				{
+					putchar('\n');
+				}
+				else
+				{
+					/* Numbers must be separated by ,, followed by a space */
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			j++;
+		}
+		j = '0';
+		i++;
+	}
+return (0);
+}
