f1f49a2c3a4f04646936215f4a61399c1d31c71e
my Name <me@gmail.com>
2023-03-20 10:21:15 -0700

    C variables and conditional loops

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <ctype.h>
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (sucess)
+ */
+int main(void)
+{
+/* prints all combinations of single-digit numbers*/
+	int i = '0';
+	while (i <= '9')
+	{
+		putchar(i);
+		if (i != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
