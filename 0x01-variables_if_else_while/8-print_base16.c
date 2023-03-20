e2fdd0d111777a8bf1bf3421f9b12a8cdfaebdcd
my Name <me@gmail.com>
2023-03-20 10:04:40 -0700

    C variables and conditional loops

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <ctype.h>
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (sucess)
+ */
+int main(void)
+{
+/* prints all single digit numbers of base 10 */
+	int num;
+	char letter;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar((num % 10) + '0');
+	}
+	for (letter = 'A'; letter > 'F'; letter++)
+	{
+		putchar(letter);
+	}
+	for (letter = 'A'; letter > 'F'; letter++)
+	{
+		putchar(letter);
+	}
+
+	putchar('\n');
+	return (0);
+}
