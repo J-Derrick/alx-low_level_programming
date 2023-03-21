b4ac3c0ab691d63c3c5182864cdb3fdf7a51e2ba
my Name <me@gmail.com>
2023-03-21 14:56:07 -0700

    These are C functions and nested loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
  * print alphabet - print alphabet in lowercase
- *and print new line 
+ * and print new line 
  *
- * return (0) Success
+ * return: void
  */
 int print_alphabet(void)
 {
 	char j;
+
 	for (j ='a'; j <= 'z'; j++)
 	{
 		_putchar(j);
 	}
 	_putchar('\n');
-return (0);
 }
