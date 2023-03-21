70b06b46ff8206f430d90268ba748830fed7b582
my Name <me@gmail.com>
2023-03-21 14:45:58 -0700

    These are C functions and nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-f#include "main.h"
+#include "main.h"
 /* betty style doc for function main goes there */
 /**
  * main - Entry point
@@ -7,13 +7,14 @@ f#include "main.h"
  */
 int main(void)
 {
-	char c[8] = "_putchar';
-	unsigned int i;
-
-	for (i=0; i < sizeof(c); i++)
-	{
-		_putchar(c(i));
-	}
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
 _putchar('\n');
 return (0);
 }
