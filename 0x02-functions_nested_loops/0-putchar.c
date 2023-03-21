8d173af9c0b58aa819150913f43303e41d042776
my Name <me@gmail.com>
2023-03-21 11:51:06 -0700

    These are C functions and nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <main.h>
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (sucess)
+ */
+int main(void)
+{
+        function _putchar
+        {
+                printf("_putchar");
+        }
+printf ('\n');
+return (0);
