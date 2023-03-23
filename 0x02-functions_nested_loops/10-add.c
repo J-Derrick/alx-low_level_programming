5a773c9826cb8d5acb3823de12a72c84ba8a70e1
my Name <me@gmail.com>
2023-03-23 14:23:23 -0700

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - add two integets
+ *
+ * @a: first input digit
+ * @b: second input digit
+ *
+ * Return:: sum of a & b
+ */
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,7 +1,9 @@
 #include "main.h"
  /**
  * print_last_digit - Prints last digit of a number
- * @n : the number to be created
+ *
+ * @n : the number as input 
+ *
  * Return: Value of the last digit of number
  */
 int print_last_digit(int n)
