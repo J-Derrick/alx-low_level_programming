ced6946c401cdd1b6b1c8521860f150b403013e1
my Name <me@gmail.com>
2023-03-24 19:24:03 -0700

    Numbers constitute the only universal language

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -15,16 +15,13 @@ void print_diagonal(int n)
 
 		for (i = 1; i <= n; i++)
 		{
-			for (j = 1; j <= n; j++)
+			for (j = 1; j < i; j++)
 			{
-				if (j == i)
-				{
-					_putchar('\\');
-				}
-				else if (j > i)
-					_putchar(' ');
+				_putchar(' ');
 			}
+			_putchar('\\');
 			_putchar('\n');
 		}
+		_putchar('\n');
 	}
 }
