1fec9e77f3e5a86319157080f231599ffe00b879
myName <myemail@gmail.com>
2023-04-28 23:06:46 -0700

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
  * _isupper - uppercase letters
  * @c: char to check
@@ -9,12 +9,8 @@
 
 int _isupper(int c)
 {
-	if (c >= 'A' && c >= 'Z')
-	{
+	if (c >= 'A' && c <= 'Z')
 		return (1);
-	}
 	else
-	{
 		return (0);
-	}
 }
